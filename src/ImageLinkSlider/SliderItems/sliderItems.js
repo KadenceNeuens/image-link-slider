13075875647e84282688aad47d93fe7848a880b6
Kadence Neuens <kadenceneuens@gmail.com>
2020-08-18 02:10:05 -0500

    Updated Readme + Cleanup

--- src/ImageLinkSlider/SliderItems/sliderItems.js
+++ src/ImageLinkSlider/SliderItems/sliderItems.js
@@ -2,32 +2,32 @@ export const sliderItems = [
     {
         title: "Image 1",
         image: "/images/1.jfif",
-        link: "/projects/Slider",
+        link: "/Link1",
     },
     {
         title: "Image 2",
         image: "/images/2.jfif",
-        link: "/projects/project_b",
+        link: "/Link2",
     },
     {
         title: "Image 3",
         image: "/images/3.jfif",
-        link: "/projects/project_c",
+        link: "/Link3",
     },
     {
         title: "Image 4",
         image: "/images/4.jfif",
-        link: "/projects/project_c",
+        link: "/Link4",
     },
     {
         title: "Image 5",
         image: "/images/5.jfif",
-        link: "/projects/project_c",
+        link: "/Link5",
     },
     {
         title: "Image 6",
         image: "/images/6.jfif",
-        link: "/projects/project_c",
+        link: "/Link6",
     },
   ];
 
